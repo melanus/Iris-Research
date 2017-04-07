8e2177b2df78c266467c2140c625d2274a766319
Matthew Lanus <matt.lanus@gmail.com>
2017-04-07 01:38:15 -0400

    Initial file

---
+++ USITv2.2.0_Adam_Mac/packages/bsif/newbsif.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <map>
+#include <iostream>
+#include <matio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <time.h>
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		cout << "Needs two arguments - 1) image 2) texture filter\n" << endl;
+	}
+
+}
